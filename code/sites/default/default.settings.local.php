fb8aeb22fa5762dd94d481a47cf40fef786c3e5c
Robert Hernandez <robert.hernandez@breaktech.com>
2016-07-23 00:24:53 -0500

    Initial commit

---
+++ code/sites/default/default.settings.local.php
@@ -0,0 +1,20 @@
+<?php
+$settings['file_temporary_path'] = '../tmp';
+$settings['file_public_path'] = 'sites/default/files';
+$settings['file_private_path'] = 'sites/default/files/private';
+
+$databases = array (
+  'default' => 
+  array (
+    'default' => 
+    array (
+      'database' => 'drupal',
+      'username' => 'container',
+      'password' => 'container',
+      'host' => 'mysql',
+      'port' => '',
+      'driver' => 'mysql',
+      'prefix' => '',
+    ),
+  ),
+);
